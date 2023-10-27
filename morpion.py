74726cefd3384c83df058a47c39416dea855b628
ThomasGEMA <thomas.goutieres.edu@groupe-gema.com>
2023-10-27 15:41:23 +0200

    Update morpion.py

--- morpion.py
+++ morpion.py
@@ -1,6 +1,11 @@
+import tkinter as tk
+
 class Morpion:
  def __init__(self, root):
         self.root = root
         self.root.title("Morpion")
         self.current_player = "X"
         self.board = [""] * 9
+
+# Création des boutons pour le morpion
+        self.buttons = [tk.Button(root, text="", font=("Helvetica", 24), width=5, height=2, command=lambda i=i: self.on_click(i)) for i in range(9)]

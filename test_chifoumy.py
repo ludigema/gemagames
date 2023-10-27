1a2714b1b5f1c18167e4f6d8252e7270248ccf37
Gamaremy <148548581+Gamaremy@users.noreply.github.com>
2023-10-27 16:37:20 +0200

    Update test_chifoumy.py

--- test_chifoumy.py
+++ test_chifoumy.py
@@ -1,1 +1,14 @@
 import pytest
+from chifoumy import gagnant
+
+def test_egalite():
+    assert gagnant("pierre","pierre") == 0,0
+    assert gagnant("ciseaux","ciseaux") == 0,0
+    assert gagnant("papier","papier") == 0,0
+
+def test_OrdiGagnant():
+    assert gagnant("ciseaux","pierre") == 0,1
+    assert gagnant("papier","ciseaux") == 0,1
+    assert gagnant("pierre","papier") == 0,1
+
+test_egalite()

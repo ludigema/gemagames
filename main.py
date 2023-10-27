266e7cc3ec35cd3cf813e7eb506d2077a7bfed35
ludivineUp <44409644+ludivineUp@users.noreply.github.com>
2023-10-27 16:03:49 +0200

    review ludi python

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
 
-def main():
+from chifoumy import shifoumi
+
+def menu():
 	print("-------------------------")
 	print("A quel jeu voulez-jouer : ?")
 	print("Entrez 1 pour le Morpion")
@@ -10,57 +11,17 @@ def main():
 	user_input = int(input("Entrer le numéro du jeu : "))
 	return user_input
 
-def shifoumi():
-	score_joueur=int()
-	score_ordinateur=int()
+def main():
+	continuer = ""
+	while continuer != "N" :
+		numero_de_selection=menu()
+		if numero_de_selection==2:
+			shifoumi()
 
-	while True:
-		print("-------------------------")
-		print("pierre - Choix A ")
-		print("feuille - Choix B")
-		print("ciseaux - Choix C")
-		joueur=str()
-		ordinateur=str()
-		Choix_du_jouer = str(input("Entrer la lettre associé à votre action : "))
-		print("-------------------------")
-		if Choix_du_jouer=='A':
-			joueur='pierre'
-		elif Choix_du_jouer=='B':
-			joueur='feuille'
-		else:
-			joueur='ciseaux'
-		Choix_de_lordi=random.randrange(1, 3)
-		
-
-		if Choix_de_lordi==0:
-			ordinateur='pierre'
-		elif Choix_de_lordi==1:
-			ordinateur='feuille'
-		else:
-			ordinateur='ciseaux'
-		print(joueur,ordinateur)
-		if joueur == ordinateur:
-			print("Égalité !")
-			
-		elif (joueur == 'pierre' and ordinateur == 'ciseaux') or \
-			(joueur == 'papier' and ordinateur == 'pierre') or \
-			(joueur == 'ciseaux' and ordinateur == 'papier'):
-				print("Le joueur gagne !")
-				score_joueur += 1
-		else:
-				print("L'ordinateur gagne !")
-				score_ordinateur += 1
-				print("-------------------------")
-		print("Le score du joueur est : ", score_joueur, " |", "Le score de l'ordinateur est :",score_ordinateur)
 		print("Voulez vous refaire une partie ? Oui (O)/ Non (N) ")
 		continuer = str(input("Entrer la lettre O ou N : "))
-		if continuer == 'N':
-			break
 
 
 if __name__ == '__main__':
-	Numero_de_selection=main()
-	if Numero_de_selection==2:
-		shifoumi()
-
+	main()
 

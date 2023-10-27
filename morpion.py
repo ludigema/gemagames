8be80ab5b2c38680a09636cc9450dffcfea057a5
ThomasGEMA <thomas.goutieres.edu@groupe-gema.com>
2023-10-27 16:20:16 +0200

    Update morpion.py

--- morpion.py
+++ morpion.py
@@ -10,6 +10,20 @@ class Morpion:
 # Création des boutons pour le morpion
         self.buttons = [tk.Button(root, text="", font=("Helvetica", 24), width=5, height=2, command=lambda i=i: self.on_click(i)) for i in range(9)]
 
+        # Placement des boutons sur la grille
         for i, button in enumerate(self.buttons):
             row, col = divmod(i, 3)
             button.grid(row=row, column=col)
+
+def check_winner(self):
+        # Vérification des lignes, colonnes et diagonales
+        for i in range(3):
+            if self.board[i] == self.board[i + 3] == self.board[i + 6] != "":
+                return True
+            if self.board[i * 3] == self.board[i * 3 + 1] == self.board[i * 3 + 2] != "":
+                return True
+        if self.board[0] == self.board[4] == self.board[8] != "":
+            return True
+        if self.board[2] == self.board[4] == self.board[6] != "":
+            return True
+        return False
